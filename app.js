8a3a4c5eba1707a5fb78c598869f26a45879848c
Viktor Radulov <golova@zeoalliance.com>
2021-03-16 11:05:32 +0200

    Fixed path for fractal resources

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('FractalSoundWeb'))
+app.use('/experiments', express.static('FractalSoundWeb'));
 
 app.use('/', indexRouter);
 app.use('/experiments/fractalSounds', fractalsRouter);
